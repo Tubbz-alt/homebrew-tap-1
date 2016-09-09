9ba4e84b0bd8958d686f7cc4449cf5cd8e614666
Rafael Pinto <rafael.pinto@olx.com>
2016-09-09 16:10:02 +0200

    Bump version plugin-selenium

--- plugin-selenium.rb
+++ plugin-selenium.rb
@@ -1,9 +1,9 @@
 class PluginSelenium < Formula
   desc "Plugin to handle browser automation using Selenium."
   homepage "https://github.com/athena-oss/plugin-selenium"
-  url "https://github.com/athena-oss/plugin-selenium/archive/v0.3.0.tar.gz"
-  version "0.3.0"
-  sha256 "8f4a4110b0329a267e5ea30722c76d98a5fb794b7adf71e324d62f191b38f595"
+  url "https://github.com/athena-oss/plugin-selenium/archive/v0.3.1.tar.gz"
+  version "0.3.1"
+  sha256 "369a514d366115ec254dce0787fa61db1f1476cd368660b8f6f8bb2d62f40228"
 
   depends_on "athena"
 
