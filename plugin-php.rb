bac06a18fa05963f3d9de3b2c610c767831947c2
Rafael Pinto <rafael.pinto@olx.com>
2016-09-09 21:01:30 +0200

    Updated sha256

--- plugin-php.rb
+++ plugin-php.rb
@@ -3,7 +3,7 @@ class PluginPhp < Formula
   homepage "https://github.com/athena-oss/plugin-php"
   url "https://github.com/athena-oss/plugin-php/archive/v0.7.0.tar.gz"
   version "0.7.0"
-  sha256 "5a1d6db4098662971e70ddcf1c6b699daa8a0f4030f5446045907af28c9cdc9d"
+  sha256 "1a614fd2227b541c252c3b007163615383a548855152e037e9410a393b82dfe8"
 
   depends_on "athena"
 
