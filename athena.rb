feb8e51ae3fbcdd418f88195f991a4b3453a36ab
Rafael Pinto <rafael.pinto@olx.com>
2016-11-07 15:52:52 +0100

    Updated shasum for athena

--- athena.rb
+++ athena.rb
@@ -3,7 +3,7 @@ class Athena < Formula
   homepage "https://github.com/athena-oss/athena"
   url "https://github.com/athena-oss/athena/archive/v0.8.0.tar.gz"
   version "0.8.0"
-  sha256 "94f866be0833008d49bdf510fd0e416932bfe8557bc34c0c80a70c248f040e12"
+  sha256 "2330ce3236dd93f365b2b918d5f91db1e8d9679583a580ba761b70f23ed524a8"
 
   def install
 	  bin.install Dir["*"]
