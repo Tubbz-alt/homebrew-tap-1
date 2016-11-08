d9f549baa458dd9efff66d077e06ca3d0baa8b8a
Rafael Pinto <rafael.pinto@olx.com>
2016-11-08 16:53:08 +0100

    Updated bashunit shasum

--- bashunit.rb
+++ bashunit.rb
@@ -3,7 +3,7 @@ class Bashunit < Formula
   homepage "https://github.com/athena-oss/bashunit"
   url "https://github.com/athena-oss/bashunit/archive/v0.3.1.tar.gz"
   version "0.3.1"
-  sha256 "f005c2573a5d9424148e2bb6e26ef51204cce4d260a7e52baf9842b17a4754f5"
+  sha256 "d44c95d8ff773fb3ff35d5ea937b87407e80c4a43acd9d6045d7965bf39474df"
 
   def install
 	  prefix.install Dir["*"]
