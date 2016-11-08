577237c4d4c8839c1df7b858c5249229b3efef13
Rafael Pinto <rafael.pinto@olx.com>
2016-11-08 11:07:39 +0100

    Added linking to post_install

--- plugin-php.rb
+++ plugin-php.rb
@@ -9,6 +9,9 @@ class PluginPhp < Formula
 
   def install
         prefix.install Dir["*"]
+  end
+
+  def post_install
         system "ln -s #{prefix} $(#{HOMEBREW_PREFIX}/bin/brew --prefix athena)/plugins/php"
   end
 end
