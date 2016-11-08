0826c4ea13fe6a327954a1f93b314c03375cbb0b
Rafael Pinto <rafael.pinto@olx.com>
2016-11-08 16:49:50 +0100

    Bumped bashunit version

--- bashunit.rb
+++ bashunit.rb
@@ -1,9 +1,9 @@
 class Bashunit < Formula
   desc "A Testing Framework for Bash"
   homepage "https://github.com/athena-oss/bashunit"
-  url "https://github.com/athena-oss/bashunit/archive/v0.3.0.tar.gz"
-  version "0.3.0"
-  sha256 "13b0fd95f5a043e3067ee6a14c9cef4874c9ec1001e6d7b19b0202366513556d"
+  url "https://github.com/athena-oss/bashunit/archive/v0.3.1.tar.gz"
+  version "0.3.1"
+  sha256 "f005c2573a5d9424148e2bb6e26ef51204cce4d260a7e52baf9842b17a4754f5"
 
   def install
 	  prefix.install Dir["*"]
