e192e44d54da1966580ec393886ae73d6d8c3ffe
Rafael Pinto <rafael.pinto@olx.com>
2016-11-18 15:26:13 +0100

    Added tap for plugin-avd

---
+++ plugin-avd.rb
@@ -0,0 +1,17 @@
+class PluginAvd < Formula
+  desc "Plugin to manage Android Virtual Devices."
+  homepage "https://github.com/athena-oss/plugin-avd"
+  url "https://github.com/athena-oss/plugin-avd/archive/v0.3.0.tar.gz"
+  version "0.3.0"
+  sha256 "6533377d1b29651340ea1393f1f464bdd5fb14fed50c75831dd738a5646da09d"
+
+  depends_on "athena"
+
+  def install
+        prefix.install Dir["*"]
+  end
+
+  def post_install
+        system "ln -s #{prefix} $(#{HOMEBREW_PREFIX}/bin/brew --prefix athena)/plugins/avd"
+  end
+end
