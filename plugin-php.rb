3561a93bcd3d30393fc9b9fbbcdd73c887a93ef9
Caio Moritz Ronchi <caiomoritz@gmail.com>
2016-12-12 17:02:24 +0100

    Bumped PHP plugin version

--- plugin-php.rb
+++ plugin-php.rb
@@ -1,9 +1,9 @@
 class PluginPhp < Formula
   desc "Plugin for Test Automation using PHP as a development language."
   homepage "https://github.com/athena-oss/plugin-php"
-  url "https://github.com/athena-oss/plugin-php/archive/v0.7.0.tar.gz"
-  version "0.7.0"
-  sha256 "1a614fd2227b541c252c3b007163615383a548855152e037e9410a393b82dfe8"
+  url "https://github.com/athena-oss/plugin-php/archive/v0.7.2.tar.gz"
+  version "0.7.2"
+  sha256 "38f407140898aa76ddeafd9d76895d4dc4101b847ed219644400aef2ae43067a"
 
   depends_on "athena"
 
