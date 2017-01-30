5b45d21a06318c9f1884abfcedd7db60b32e8e95
Caio Moritz Ronchi <caiomoritz@gmail.com>
2017-01-30 09:55:19 +0100

    Upgraded athena to 0.10.2 (really)

--- athena.rb
+++ athena.rb
@@ -2,7 +2,7 @@ class Athena < Formula
   desc "An automation platform with a plugin architecture that allows you to easily create and share services."
   homepage "https://github.com/athena-oss/athena"
   url "https://github.com/athena-oss/athena/archive/v0.10.2.tar.gz"
-  version "0.10.1"
+  version "0.10.2"
   sha256 "de9628f1ad19671105f4fa87b893212acc4509a925fa23b5db3569e7f8aa52fb"
 
   def install
