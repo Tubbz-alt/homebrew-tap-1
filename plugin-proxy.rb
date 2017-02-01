d6849408bb7cdd4ec191e4597874ce2d68af9959
Caio Moritz Ronchi <caiomoritz@gmail.com>
2017-02-01 17:31:46 +0100

    Upgraded the Proxy Plugin to 0.3.2

--- plugin-proxy.rb
+++ plugin-proxy.rb
@@ -1,9 +1,9 @@
 class PluginProxy < Formula
   desc "Plugin to handle browser automation using Selenium."
   homepage "https://github.com/athena-oss/plugin-proxy"
-  url "https://github.com/athena-oss/plugin-proxy/archive/v0.3.1.tar.gz"
-  version "0.3.1"
-  sha256 "246973ee4b523da534d3f0e81246ab9d4e62c9cbaacd6a86338875c88c620bbb"
+  url "https://github.com/athena-oss/plugin-proxy/archive/v0.3.2.tar.gz"
+  version "0.3.2"
+  sha256 "9fa240a2b0e9f365c3c2210cb7f92e3d756848478ff47cb663f870b1a4c52a87"
 
   depends_on "athena"
 
