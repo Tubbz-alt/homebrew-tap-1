ac6af788188241dc181224ec859f7ad05b8fac4c
Caio Moritz Ronchi <caiomoritz@gmail.com>
2017-02-01 16:20:12 +0100

    Updated athena formula to 0.10.3

--- athena.rb
+++ athena.rb
@@ -1,9 +1,9 @@
 class Athena < Formula
   desc "An automation platform with a plugin architecture that allows you to easily create and share services."
   homepage "https://github.com/athena-oss/athena"
-  url "https://github.com/athena-oss/athena/archive/v0.10.2.tar.gz"
-  version "0.10.2"
-  sha256 "de9628f1ad19671105f4fa87b893212acc4509a925fa23b5db3569e7f8aa52fb"
+  url "https://github.com/athena-oss/athena/archive/v0.10.3.tar.gz"
+  version "0.10.3"
+  sha256 "d07197b94c9cb79cd1d37815604e19d3519074e316719ff91182de22e98c18bd"
 
   def install
 	  prefix.install Dir["*"]
