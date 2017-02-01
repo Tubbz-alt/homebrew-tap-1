0b57c2010bdc75fa7f867199b706279420f7393f
Caio Moritz Ronchi <caiomoritz@gmail.com>
2017-02-01 17:40:15 +0100

    Fixed Proxy Plugin SHA sum

--- plugin-proxy.rb
+++ plugin-proxy.rb
@@ -3,7 +3,7 @@ class PluginProxy < Formula
   homepage "https://github.com/athena-oss/plugin-proxy"
   url "https://github.com/athena-oss/plugin-proxy/archive/v0.3.2.tar.gz"
   version "0.3.2"
-  sha256 "9fa240a2b0e9f365c3c2210cb7f92e3d756848478ff47cb663f870b1a4c52a87"
+  sha256 "199e1c304c6b97346a218ed2f79fe1826f2f4bfe7e26aff838df539fceee16ee"
 
   depends_on "athena"
 
