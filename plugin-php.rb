0fd7038094513767cc03866ab1b4534a9be79975
Caio Moritz Ronchi <caiomoritz@gmail.com>
2017-02-01 17:29:44 +0100

    Upgraded the PHP Plugin to 0.7.4

--- plugin-php.rb
+++ plugin-php.rb
@@ -1,9 +1,9 @@
 class PluginPhp < Formula
   desc "Plugin for Test Automation using PHP as a development language."
   homepage "https://github.com/athena-oss/plugin-php"
-  url "https://github.com/athena-oss/plugin-php/archive/v0.7.2.tar.gz"
-  version "0.7.2"
-  sha256 "38f407140898aa76ddeafd9d76895d4dc4101b847ed219644400aef2ae43067a"
+  url "https://github.com/athena-oss/plugin-php/archive/v0.7.4.tar.gz"
+  version "0.7.4"
+  sha256 "7d23c096959fae9d5703700fb8a142d9b5824d684b004dd0f8b92d792c873f0d"
 
   depends_on "athena"
 
