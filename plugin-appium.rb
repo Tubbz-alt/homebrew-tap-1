07f2f925ccd250e5cff7dc6730e89533e38c050d
Caio Moritz Ronchi <caiomoritz@gmail.com>
2017-07-17 15:55:27 +0200

    appium plugin version bump: 0.4.0

--- plugin-appium.rb
+++ plugin-appium.rb
@@ -1,9 +1,9 @@
 class PluginAppium < Formula
   desc "Plugin to handle mobile automation using Appium."
   homepage "https://github.com/athena-oss/plugin-appium"
-  url "https://github.com/athena-oss/plugin-appium/archive/v0.3.0.tar.gz"
-  version "0.3.0"
-  sha256 "bff16f671fc921532e14b265e70dc5fb8fe25494deec6fcac272885980f16a47"
+  url "https://github.com/athena-oss/plugin-appium/archive/v0.4.0.tar.gz"
+  version "0.4.0"
+  sha256 "f9a802cdaa13f1c9ef7708e565437a0a339b805a10beac2bdd5759f6dc033f69"
   keg_only "Not meant for standalone usage - this is an Athena plugin"
 
   depends_on "athena"
