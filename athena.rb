0e78a88c57bc379e2b55d44f11094249984da6e4
Caio Moritz Ronchi <caiomoritz@gmail.com>
2017-07-17 11:22:49 +0200

    athena version bump: 0.11.0

--- athena.rb
+++ athena.rb
@@ -1,9 +1,9 @@
 class Athena < Formula
   desc "An automation platform with a plugin architecture that allows you to easily create and share services."
   homepage "https://github.com/athena-oss/athena"
-  url "https://github.com/athena-oss/athena/archive/v0.10.4.tar.gz"
-  version "0.10.4"
-  sha256 "8865880edb0e9bfdcb912dcf8218e02f09fca04e516f9d8124d38fafc5a78319"
+  url "https://github.com/athena-oss/athena/archive/v0.11.0.tar.gz"
+  version "0.11.0"
+  sha256 "346116e7c9bdc47a6d15b7d4a6c00f6eee03e188a85d372770be0dcdf67d1996"
 
   def install
 	  prefix.install Dir["*"]
