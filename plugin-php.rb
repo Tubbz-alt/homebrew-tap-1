ce6c14732a10f1d62308b228845f1ffabce70c08
Simon Effenberg <simon.effenberg@olx.com>
2018-06-25 09:55:29 +0200

    new php plugin version

--- plugin-php.rb
+++ plugin-php.rb
@@ -2,8 +2,8 @@ class PluginPhp < Formula
   desc "Plugin for Test Automation using PHP as a development language."
   homepage "https://github.com/athena-oss/plugin-php"
   url "https://github.com/athena-oss/plugin-php/archive/v0.8.2.tar.gz"
-  version "0.8.2"
-  sha256 "506a27708957a7bcfb42eea44fda42d7a8b3dbab4a4e4640fd04ff2b38348949"
+  version "0.8.3"
+  sha256 "611b19ebd4c76cf26bc5e0330cf6ffc412734027a1715fb0340c2be6aab498d3"
   keg_only "Not meant for standalone usage - this is an Athena plugin"
 
   depends_on "athena"
